2c213b159137d6af0e11e793ce3756f69ee5311c
Steve Good <sgood@lanctr.com>
2017-07-30 15:27:15 -0700

    Fixed some code style issues being reported by codacy.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const conditions = require('../xwing-data/data/conditions.js')
-const damageDeckCore = require('../xwing-data/data/damage-deck-core.js')
-const damageDeckCoreTfa = require('../xwing-data/data/damage-deck-core-tfa.js')
-const damageDeckRebelTransport = require('../xwing-data/data/damage-deck-rebel-transport.js')
-const pilots = require('../xwing-data/data/pilots.js')
-const referenceCards = require('../xwing-data/data/reference-cards.js')
-const ships = require('../xwing-data/data/ships.js')
-const sources = require('../xwing-data/data/sources.js')
-const upgrades = require('../xwing-data/data/upgrades.js')
+const conditions = require("../xwing-data/data/conditions.js");
+const damageDeckCore = require("../xwing-data/data/damage-deck-core.js");
+const damageDeckCoreTfa = require("../xwing-data/data/damage-deck-core-tfa.js");
+const damageDeckRebelTransport = require("../xwing-data/data/damage-deck-rebel-transport.js");
+const pilots = require("../xwing-data/data/pilots.js");
+const referenceCards = require("../xwing-data/data/reference-cards.js");
+const ships = require("../xwing-data/data/ships.js");
+const sources = require("../xwing-data/data/sources.js");
+const upgrades = require("../xwing-data/data/upgrades.js");
 
 const data = {
   conditions,
@@ -18,7 +18,7 @@ const data = {
   ships,
   sources,
   upgrades
-}
+};
 
 module.exports = {
   conditions,
@@ -30,4 +30,4 @@ module.exports = {
   ships,
   sources,
   upgrades
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-var path = require('path')
+var path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'xwing-data-module.js'
+    path: path.resolve(__dirname, "lib"),
+    filename: "xwing-data-module.js"
   },
   module: {
     rules: [{
       test: /\.js$/,
       exclude: /(node_modules|xwing-data)/,
       use: {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['env']
+          presets: ["env"]
         }
       }
     }, {
       test: /xwing\-data\.js$/,
       exclude: /node_modules/,
       use: {
-        loader: 'file-loader'
+        loader: "file-loader"
       }
     }]
   }
-}
+};
